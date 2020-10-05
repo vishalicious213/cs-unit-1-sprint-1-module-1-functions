221b0ac2a47f04ac78f077655da9e69a88f7b11b
Vish Singh <vishalicious213@gmail.com>
2020-10-05 21:59:10 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+"""
+Modify this function to make it return the sum of the arguments a and b.
+"""
+def sum(a, b):
+    # DELETE THE PASS STATEMENT AND WRITE YOUR CODE HERE
+    # pass
+    return a + b
+
+# This should print 7
+print(sum(2, 5))
+
+
+"""
+Modify this function to use the sum function above to return
+the double of the sum of a and b.
+"""
+def double_the_sum(a, b):
+    # DELETE THE PASS STATEMENT AND WRITE YOUR CODE HERE
+    # pass
+    return sum(a, b) * 2
+
+# This should print 14
+print(double_the_sum(2, 5))
